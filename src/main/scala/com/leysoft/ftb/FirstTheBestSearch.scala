c1f2db3b1d70ccebc46c849fc5cd7a30462ac999
Will Leyton <wleyton@addi.com>
2019-08-26 21:44:41 -0500

    Add First the best search

--- src/main/scala/com/leysoft/ftb/FirstTheBestSearch.scala
+++ src/main/scala/com/leysoft/ftb/FirstTheBestSearch.scala
@@ -1,4 +1,4 @@
-package com.leysoft.astar
+package com.leysoft.ftb
 
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
@@ -7,36 +7,30 @@ import scala.collection.mutable.{PriorityQueue, Set}
 import scala.math.{abs, pow, sqrt}
 import util.control.Breaks._
 
-class AstarSearch {
+class FirstTheBestSearch {
 
   def search(source: Node, target: Node): (Boolean, List[Node]) = {
     var search = false
     val explored: Set[Node] = Set.empty
-    var unxplored = PriorityQueue[Node]()(AstarSearch.ordering)
-    source.h = AstarSearch.manhattan(source, target)
-    source.f = source.g + source.h
+    var unxplored = PriorityQueue[Node]()(FirstTheBestSearch.ordering)
     unxplored.enqueue(source)
 
     while (!unxplored.isEmpty && !search) {
       val current = unxplored.dequeue
       explored.add(current)
-      AstarSearch.logger.info(s"Current node: $current")
-      if(current.value.equals(target.value)) {
-        AstarSearch.logger.info("Eureka")
+      FirstTheBestSearch.logger.info(s"Current node: $current")
+      if (current.value.equals(target.value)) {
         search = true
       } else {
         for (edge <- current.neighbors) {
           val child = edge.node
-          val weight = edge.weight
-          val g = current.g + weight
-          val h = AstarSearch.manhattan(child, target)
-          val f = g + h
+          val h = FirstTheBestSearch.manhattan(child, target)
+          val f = h
           breakable {
             if (explored.contains(child) && f >= child.f) {
               break
             } else if (!unxplored.exists(node => node.equals(child)) || f < child.f) {
               child.parent = Some(current)
-              child.g = g
               child.h = h
               child.f = f
               if (unxplored.exists(node => node.equals(child)))
@@ -61,9 +55,9 @@ class AstarSearch {
   }
 }
 
-object AstarSearch {
+object FirstTheBestSearch {
 
-  val logger = Logger(LoggerFactory.getLogger("AstarSearch"))
+  val logger = Logger(LoggerFactory.getLogger("FirstTheBestSearch"))
 
   val ordering: Ordering[Node] = (n1: Node, n2: Node) => -n1.f.compare(n2.f)
 
@@ -73,17 +67,17 @@ object AstarSearch {
   // Euclidean distance
   val euclidean = (n1: Node, n2: Node) => sqrt(pow(n2.x - n1.x, 2) + pow(n2.y - n1.y, 2))
 
-  def apply(): AstarSearch = new AstarSearch()
+  def apply(): FirstTheBestSearch = new FirstTheBestSearch()
 }
 
-case class Node(value: String, x: Double, y: Double, var g: Double = 0, var h: Double = 0,
-                var f: Double = 0, var parent: Option[Node] = None, var block: Boolean = false)  {
+case class Node(value: String, x: Double, y: Double, var h: Double = 0, var f: Double = 0,
+                var parent: Option[Node] = None, var block: Boolean = false)  {
 
   var neighbors: List[Edge] = List.empty
 
   def add(edge: Edge): Unit = neighbors = neighbors.appended(edge)
 
-  override def toString: String = s"Node($value, $x, $y, $g, $h, $f)"
+  override def toString: String = s"Node($value, $x, $y, $h, $f)"
 
   override def equals(obj: Any): Boolean = {
     val other = obj.asInstanceOf[Node]
@@ -91,10 +85,10 @@ case class Node(value: String, x: Double, y: Double, var g: Double = 0, var h: D
   }
 }
 
-case class Edge(node: Node, var weight: Double = 0)
+case class Edge(node: Node)
 
-object Astar extends App {
-  val logger = Logger(LoggerFactory.getLogger("Astar"))
+object FirstTheBest extends App {
+  val logger = Logger(LoggerFactory.getLogger("FirstTheBest"))
 
   val a = Node("a", 1, 2)
   val b = Node("b", 3, 0)
@@ -103,12 +97,12 @@ object Astar extends App {
   val e = Node("e", 5, 3)
   val f = Node("f", 3, 6)
 
-  a.add(Edge(b, 3)); a.add(Edge(c, 3))
-  b.add(Edge(c, 4)); b.add(Edge(d, 3))
-  c.add(Edge(e, 7))
-  d.add(Edge(e, 3)); d.add(Edge(f, 6))
-  e.add(Edge(f, 2))
+  a.add(Edge(b)); a.add(Edge(c))
+  b.add(Edge(c)); b.add(Edge(d))
+  c.add(Edge(e))
+  d.add(Edge(e)); d.add(Edge(f))
+  e.add(Edge(f))
 
-  val result = AstarSearch().search(a, e)
+  val result = FirstTheBestSearch().search(a, e)
   logger.info(s"Search success: ${result._1}, result: ${result._2}")
 }
--- src/main/scala/com/leysoft/idfs/IterativeDepthFirstSearch.scala
+++ src/main/scala/com/leysoft/idfs/IterativeDepthFirstSearch.scala
@@ -31,7 +31,6 @@ class IterativeDepthFirstSearch {
         }
       }
     }
-
     false
   }
 }
